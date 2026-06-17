8b525cc82dee16cf071c07f2dfacb14fafdbda2f
Lingfeng Zhang <zhanglingfeng04@meituan.com>
2026-06-17 21:20:14 +0000

    fix: precompute exposed CORS headers

--- internal/api/server.go
+++ internal/api/server.go
@@ -61,6 +61,8 @@ var corsExposedResponseHeaders = []string{
 	"X-SERVER-BUILD-DATE",
 }
 
+var corsExposedResponseHeadersJoined = strings.Join(corsExposedResponseHeaders, ", ")
+
 type serverOptionConfig struct {
 	extraMiddleware      []gin.HandlerFunc
 	engineConfigurator   func(*gin.Engine)
@@ -1483,7 +1485,7 @@ func corsMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Expose-Headers", strings.Join(corsExposedResponseHeaders, ", "))
+		c.Header("Access-Control-Expose-Headers", corsExposedResponseHeadersJoined)
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
